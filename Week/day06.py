a3354f34dc5e81e9af0827a19437c2bf4b262ae9
Cybebe <kishandesailife@gmail.com>
2025-08-19 05:24:27 -0700

    day10

--- Week/day06.py
+++ Week/day06.py
@@ -6,6 +6,8 @@ search_query = "strivekishan dsa day in life day wokring okay  "
 # Create the Google search URL
 url = f"https://www.youtube.com/search?q={search_query}"
 
+print("its working okay ")
+
 # Open in Google Chrome
 # Optional: Set path to Chrome if default browser is not Chrome
 chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
